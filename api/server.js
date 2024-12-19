75bfc2aa95925a23f402ca126da4398e48a8687c
misganawtad <misganawt@gmail.com>
2024-12-19 09:36:00 +0900

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,23 +1,9 @@
-const jsonServer = require('json-server');
-const path = require('path');
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
+server.use(middlewares)
+server.use(router)
 
-// Add this middleware to handle CORS and other issues
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  next();
-});
-
-server.use(router);
-
-<<<<<<< HEAD
-module.exports = server;
-=======
-module.exports = server;
->>>>>>> bcacb26ec6e7438e912e96e438a9cfb9e19627d1
+module.exports = server
